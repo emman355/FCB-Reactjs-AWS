b7add4fb50ef102f9a7df5f86666472204cf5798
Earevalo355 <earevalo355@gmail.com>
2022-01-20 16:40:14 +0800

    add a margin in <p>

--- src/index.js
+++ src/index.js
@@ -6,11 +6,19 @@ import "./index.css";
 // my first component
 
 const App = () => {
+    const paraStyle = {
+        "marginBottom": "15px",
+    };
     return (
         <div className="app">
             <header className="app-header">
-                <h1>My Amazing ToDo-List App</h1>
-                <p>The most simple and amazing todo-list React App.</p>
+                <h1 >My Amazing ToDo-List App</h1>
+                <p style={paraStyle}>The most simple and amazing todo-list React App.</p>
+                <ul>
+                    <li className="tasks-item">Follow Education.ph on Facebook</li>
+                    <li className="tasks-item">Follow Education.ph on Facebook</li>
+                    <li className="tasks-item">Follow Education.ph on Facebook</li>
+                </ul>
             </header>
         </div>
         
